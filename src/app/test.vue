246fbf20e5aa8bce81e41bbfc943f56a7894d253
haiwennnn <haiwengoodboy@126.com>
2017-10-13 01:12:49 +0800

    no message

---
+++ src/app/test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="zz-body">
+    <div class="zz-body-wrap">
+      <div class="flex-demo">
+        <div class="zz-flex">
+          <div class="zz-flex-item">test</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'test'
+}
+</script>
+<style lang="less">
+</style>
